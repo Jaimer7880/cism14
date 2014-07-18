96d3fcef11b4a03e510f019f9653d26025c398cc
jaimer7880 <jgarbe02@seattlecentral.edu>
2014-07-17 18:51:30 -0700

    Adding new bootswatch theme

---
+++ application/config/custom_config.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//custom_config.php - a place to save global settings
+
+$config['style'] = 'flatly.css';
+$config['banner'] = 'Default banner';
+$config['title'] = 'Default title';
+$config['copyright'] = 'Default copyright';
+$config['theme'] = '';
